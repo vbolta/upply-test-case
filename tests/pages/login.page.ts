10eb5c3f98f911f8f52ece26035b889b0d14c5fd
Victor Bolta <victor.bolta@gmail.com>
2025-04-18 22:57:46 +0200

    Login test case

--- tests/pages/login.page.ts
+++ tests/pages/login.page.ts
@@ -3,7 +3,6 @@ import { type Locator, type Page } from "@playwright/test";
 export class loginPage {
   readonly page: Page;
   readonly cookiesConsentButton: Locator;
-  readonly signUpLoginLink: Locator;
   readonly signUpNameTextbox: Locator;
   readonly signUpEmailTextbox: Locator;
   readonly signUpButton: Locator;
@@ -17,11 +16,13 @@ export class loginPage {
   readonly zipcodeTextbox: Locator;
   readonly mobileNumberTextbox: Locator;
   readonly createAccountButton: Locator;
+  readonly loginEmailTextbox: Locator;
+  readonly loginPasswordTextbox: Locator;
+  readonly loginButton: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.cookiesConsentButton = page.getByRole("button", { name: "Consent" });
-    this.signUpLoginLink = page.locator('[href="/login"]');
     this.signUpNameTextbox = page.getByTestId("signup-name");
     this.signUpEmailTextbox = page.getByTestId("signup-email");
     this.signUpButton = page.getByTestId("signup-button");
@@ -35,16 +36,27 @@ export class loginPage {
     this.zipcodeTextbox = page.getByTestId("zipcode");
     this.mobileNumberTextbox = page.getByTestId("mobile_number");
     this.createAccountButton = page.getByTestId("create-account");
+    this.loginEmailTextbox = page.getByTestId("login-email");
+    this.loginPasswordTextbox = page.getByTestId("login-password");
+    this.loginButton = page.getByTestId("login-button");
   }
 
   async goto() {
-    await this.page.goto("https://www.automationexercise.com/");
+    await this.page.goto("https://www.automationexercise.com/login");
+    // An improvement would be to directly add the cookies needed instead of clicking on the button
+    await this.cookiesConsentButton.click();
   }
 
-  async newUserSignup(firstName: string, lastName: string, email: string) {
+  async signup({
+    firstName,
+    lastName,
+    email,
+  }: {
+    firstName: string;
+    lastName: string;
+    email: string;
+  }) {
     await this.goto();
-    await this.cookiesConsentButton.click();
-    await this.signUpLoginLink.click();
     await this.signUpNameTextbox.fill(`${firstName} ${lastName}`);
     await this.signUpEmailTextbox.fill(email);
     await this.signUpButton.click();
@@ -70,5 +82,14 @@ export class loginPage {
     await this.zipcodeTextbox.fill(zipcode);
     await this.mobileNumberTextbox.fill(phoneNumber);
     await this.createAccountButton.click();
+  }
+
+  async login({ email, password }: { email: string; password: string }) {
+    await this.goto();
+    await this.loginEmailTextbox.click();
+    await this.loginEmailTextbox.fill(email);
+    await this.loginPasswordTextbox.click();
+    await this.loginPasswordTextbox.fill(password);
+    await this.loginButton.click();
   }
 }
--- tests/testCase.spec.ts
+++ tests/testCase.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test";
+import { test, expect, chromium } from "@playwright/test";
 import { userDetails } from "tests/helper/userDetails";
 import { loginPage } from "tests/pages/login.page";
 
@@ -14,9 +14,14 @@ const {
   phoneNumber,
 } = userDetails;
 
+// test.beforeEach(async({page}) => {
+//   const login = new loginPage(page);
+//   await login.goto();
+// })
+
 test("Register User", async ({ page }) => {
   const login = new loginPage(page);
-  await login.newUserSignup(firstName, lastName, email);
+  await login.signup({ firstName, lastName, email });
   await login.fillAccountInformation(
     firstName,
     lastName,
@@ -30,13 +35,41 @@ test("Register User", async ({ page }) => {
   await expect(page.getByText("Account Created!")).toBeVisible();
 });
 
-// test("Login", async ({ page }) => {
+test("Login", async ({ page }) => {
+  const login = new loginPage(page);
+  await login.login({ email, password });
+  await expect(
+    page.getByText(`Logged in as ${firstName} ${lastName}`),
+  ).toBeVisible();
+});
 
+// await page.goto('https://www.automationexercise.com/account_created');
 // await page.getByRole('link', { name: ' Signup / Login' }).click();
-// await page.locator('form').filter({ hasText: 'Login' }).getByPlaceholder('Email Address').click();
-// await page.locator('form').filter({ hasText: 'Login' }).getByPlaceholder('Email Address').fill('tess');
-// await page.getByRole('textbox', { name: 'Password' }).click();
-// await page.getByRole('textbox', { name: 'Password' }).fill('testt');
-// await page.getByRole('button', { name: 'Login' }).click();
-
-// })
+// await page.getByRole('link', { name: ' Products' }).click();
+// await page.getByRole('textbox', { name: 'Search Product' }).click();
+// await page.getByRole('textbox', { name: 'Search Product' }).fill('T-Shirt');
+// await page.getByRole('button', { name: '' }).click();
+// await page.getByRole('link', { name: '(3) Mast & Harbour' }).click();
+// await page.locator('div:nth-child(5) > .product-image-wrapper > .single-products > .product-overlay > .overlay-content > .btn').click();
+// await page.getByRole('link', { name: 'View Cart' }).click();
+// await page.getByRole('button', { name: '1' }).click();
+// await page.getByRole('button', { name: '1' }).click();
+// await page.getByRole('cell', { name: '' }).locator('a').click();
+// await page.getByRole('link', { name: ' Products' }).click();
+// await page.getByRole('textbox', { name: 'Search Product' }).click();
+// await page.getByRole('textbox', { name: 'Search Product' }).fill('T-Shirt');
+// await page.getByRole('button', { name: '' }).click();
+// await page.getByRole('link', { name: '(3) Mast & Harbour' }).click();
+// await page.getByRole('link', { name: ' View Product' }).nth(2).click();
+// await page.locator('#quantity').click();
+// await page.locator('#quantity').fill('3');
+// await page.getByRole('button', { name: ' Add to cart' }).click();
+// await page.getByRole('link', { name: 'View Cart' }).click();
+// await page.getByText('Proceed To Checkout').click();
+// await page.getByRole('link', { name: 'Place Order' }).click();
+// await page.getByTestId('name-on-card').click();
+// await page.getByTestId('card-number').click();
+// await page.getByTestId('cvc').click();
+// await page.getByTestId('expiry-month').click();
+// await page.getByTestId('expiry-year').click();
+// await page.getByTestId('pay-button').click();
